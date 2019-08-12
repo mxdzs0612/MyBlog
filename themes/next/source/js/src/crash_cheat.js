af4fc7980d43b890976d29e72051ae83799b4400
jmy <jiaomy@hust.edu.cn>
2019-08-12 09:55:36 +0800

    网站图标

--- themes/next/source/js/src/crash_cheat.js
+++ themes/next/source/js/src/crash_cheat.js
@@ -3,12 +3,12 @@
  var titleTime;
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
-         $('[rel="icon"]').attr('href', "/img/TEP.ico");
+         $('[rel="icon"]').attr('href', "/images/favicon.ico");
          document.title = '大哥别走~！！';
          clearTimeout(titleTime);
      }
      else {
-         $('[rel="icon"]').attr('href', "/favicon.ico");
+         $('[rel="icon"]').attr('href', "/images/favicon-16x16-next.png");
          document.title = '回来就好回来就好……' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
